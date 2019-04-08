1ae6194a28f84c41f8d0fba4e979eafed80d76bb
apqx <changgongapq@gmail.com>
2019-04-09 01:29:49 +0800

    Scroller, common sync

--- app/src/main/java/me/apqx/demo/widget/view/scroll/ScrollableView.kt
+++ app/src/main/java/me/apqx/demo/widget/view/scroll/ScrollableView.kt
@@ -5,21 +5,30 @@ import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import android.widget.ImageView
+import android.widget.Scroller
 import androidx.constraintlayout.widget.ConstraintLayout
 import me.apqx.demo.LogUtil
 import java.util.jar.Attributes
 
-class ScalableView : ImageView {
+/**
+ * 跟随手指移动的View，5种移动方式
+ */
+class ScrollableView : ImageView {
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
-    var curX = 0f
-    var curY = 0f
-    var lastX = 0f
-    var lastY = 0f
-    var offsetX = 0f
-    var offsetY = 0f
+    private val scroller = Scroller(context)
+
+    private var curX = 0f
+    private var curY = 0f
+    private var lastX = 0f
+    private var lastY = 0f
+    private var offsetX = 0f
+    private var offsetY = 0f
+
+
+
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         curX = event!!.rawX
         curY = event!!.rawY
@@ -33,11 +42,18 @@ class ScalableView : ImageView {
                 offsetX = curX - lastX
                 offsetY = curY - lastY
                 // 进行重新定位，高频的重定位将表现为滑动
-                location5(offsetX.toInt(), offsetY.toInt())
+                location4(offsetX.toInt(), offsetY.toInt())
 
                 lastX = curX
                 lastY = curY
 
+            }
+
+            MotionEvent.ACTION_UP -> {
+                // 当手指移开的时候，平滑移动到原点
+                val parentView = parent as View
+                scroller.startScroll(parentView.scrollX, parentView.scrollY, -parentView.scrollX, -parentView.scrollY, 1000)
+                invalidate()
             }
         }
         return true
@@ -62,12 +78,22 @@ class ScalableView : ImageView {
     }
 
     private fun location4(offsetX: Int, offsetY: Int) {
-        (parent as View).scrollBy(-offsetX, - offsetY)
+        val parentView = parent as View
+        parentView.scrollBy(-offsetX, - offsetY)
     }
 
     private fun location5(offsetX: Int, offsetY: Int) {
-        LogUtil.d("offsetX = $offsetX, offsetY = $offsetY")
         val parentView = parent as View
-        parentView.scrollTo(parentView.x.toInt() + offsetX, parentView.y.toInt() + offsetY)
+        LogUtil.d("offsetX = $offsetX, offsetY = $offsetY")
+        parentView.scrollTo(parentView.scrollX - offsetX, parentView.scrollY - offsetY)
+    }
+
+    override fun computeScroll() {
+        val parentView = parent as View
+        super.computeScroll()
+        if (scroller.computeScrollOffset()) {
+            parentView.scrollTo(scroller.currX, scroller.currY)
+            postInvalidate()
+        }
     }
 }
